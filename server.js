f4df9d051c743dac27c49f950240c164c5537d23
jairo-omnipro <jairo.guevara@omni.pro>
2022-09-06 06:01:29 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express'); // importacion del modulo express
+
+const app = express();              // creacion del objeto express() para configurar la API REST.
+
+const PORT     = 3000;              // puerto en donde se montara el servicio de la API REST 
+const HOSTNAME = "127.0.0.1";       // IP donde se montara el servicio de la API REST (en este caso, localhost)
+
+                                    // URL de la API: http://localhost:3000
+
+/**
+ * IMPORTACION DE ROUTERS INDICES
+ * se importan las rutas de los index.js ubicados en cada carpeta de routes, tanto de la parte web como la mobile
+ */
+
+
+
+app.use(express.json());                         // se prepara el servidor para que pueda recibir datos JSON de los request que se hagan
+                                                 // desde el cliente
+
+
+                                                 
+/**
+ *  Se monta el servidor Node para que escuche las peticiones en el puerto PORT, y la IP HOSTNAME
+ *  Se envia un mensaje por consola para indicar que el servidor esta corriendo
+ */
+app.listen(PORT, HOSTNAME, () => {
+    console.log("El servidor esta ON");
+});
